ac6bf0b51888d5bd12a293de878399564a24a4bb
Rachel Munoz <rachelumunoz@gmail.com>
2017-01-23 17:16:53 -0800

    Adds env varaible for production db, without typo

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var mongoose = require('mongoose');
 
 
 // var dbURL = 'mongodb://localhost:27017/yakyik'
-mongoose.connect(process.ENV.MONGODB_URI, function(err, res){
+mongoose.connect(process.env.MONGODB_URI, function(err, res){
   if(err){
     console.log('DB connection failed', err)
   }else {
