f2bbd8b74dcb316e7b785ba163895116a86e4e3a
Rachel Munoz <rachelumunoz@gmail.com>
2017-01-23 16:10:19 -0800

    Uncomments routes to add zones

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+
+
 var dbURL = 'mongodb://localhost:27017/yakyik'
 mongoose.connect(dbURL, function(err, res){
   if(err){
@@ -26,8 +28,8 @@ app.use(express.static('public'));
 
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hjs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -40,11 +42,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', api);
-// app.use('/', index);
+app.use('/', index);
 
-app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, '/public/', 'index.html'))
-})
+// app.get('*', (req, res)=>{
+//   res.sendFile(path.join(__dirname, '/public/', 'index.html'))
+// })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('*', function(req, res, next){
-  res.sendFile(path.join(__dirname + '/views/', 'index.hjs'))
-})
+// router.get('*', function(req, res, next){
+//   res.sendFile(path.join(__dirname + '/views/', 'index.hjs'))
+// })
 
 router.get('/', function(req, res, next) {
   res.render('index.html', null);
