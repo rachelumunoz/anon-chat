4829e41f5adcc40000a5da5200f2763e1a1b34e1
Rachel Munoz <rachelumunoz@gmail.com>
2017-02-16 19:26:53 -0800

    Add mlab uri

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 
-
-var dbURL = 'mongodb://localhost:27017/yakyik'
-mongoose.connect(dbURL, function(err, res){
+// var dbURL = 'mongodb://localhost:27017/yakyik'
+mongoose.connect(MONGODB_URI, function(err, res){
   if(err){
     console.log('DB connection failed', err)
   }else {
